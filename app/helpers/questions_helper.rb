f7df4330c552a1356328c4744a7527430f36c38b
Łukasz Gurdek <git@ukasiu.pl>
2015-07-21 21:26:36 +0200

    fixed latex rendering within markdown

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,11 @@
 module QuestionsHelper
+  def render_markdown(markdown)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(hard_wrap: true), autolink: true, tables: true)
+    saved_groups = []
+    @markdown.render(markdown.gsub(/\^\^(.*)\^\^/) do |s| # a dirty hack to disable markdown rendering between ^^
+      saved_groups << s
+      "##group-#{saved_groups.size - 1}##"
+    end).gsub(/##group-(\d)##/) {|_| saved_groups[$1.to_i]}.html_safe
+  end
+
 end
