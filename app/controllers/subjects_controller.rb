bac94aaa31e332d2032763e4b14784b25565bfa8
Łukasz Gurdek <git@ukasiu.pl>
2015-07-22 21:32:05 +0200

    subject tiles WIP

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -4,7 +4,7 @@ class SubjectsController < ApplicationController
   # GET /subjects
   # GET /subjects.json
   def index
-    @subjects = Subject.all
+    @subjects = Subject.all.sort_by{|s| (current_user.current_semester - s.study_semester).abs}
   end
 
   # GET /subjects/1
@@ -12,45 +12,9 @@ class SubjectsController < ApplicationController
   def show
   end
 
-  # GET /subjects/new
-  def new
-    @subject = Subject.new
-  end
-
-  # POST /subjects
-  # POST /subjects.json
-  def create
-    @subject = Subject.new(subject_params)
-
-    respond_to do |format|
-      if @subject.save
-        format.html { redirect_to @subject, notice: 'Subject was successfully created.' }
-        format.json { render :show, status: :created, location: @subject }
-      else
-        format.html { render :new }
-        format.json { render json: @subject.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /subjects/1
-  # DELETE /subjects/1.json
-  def destroy
-    @subject.destroy
-    respond_to do |format|
-      format.html { redirect_to subjects_url, notice: 'Subject was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_subject
       @subject = Subject.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def subject_params
-      params.require(:subject).permit(:name, :study_semester)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User
   field :username, type: String
   field :first_name, type: String
   field :last_name, type: String
+  field :start_year, type: Integer # TODO retrieve this from API
   field :accounts_api_id, type: String
 
   field :admin, type: Mongoid::Boolean, default: false
@@ -39,6 +40,10 @@ class User
 
   def full_name
     "#{first_name} #{last_name} (#{username})"
+  end
+
+  def current_semester
+    (Time.now.year-start_year) * 2 + (Time.now.month >= 10 ? 1 : 0)
   end
 
 end
